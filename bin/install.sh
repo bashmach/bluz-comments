a56a5ee897e5d795902cb6876f4ddc110adc74b0
Pavel Machekhin <pavel.machekhin@gmail.com>
2012-12-10 19:50:28 +0200

    Removed css and js paths and variables from install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -3,12 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 APP="$( realpath "$DIR/../../../../application" )"
-CSS="$( realpath "$DIR/../../../../public/js" )"
-JS="$( realpath "$DIR/../../../../public/css" )"
-
 SRC="$(realpath $DIR/../src)"
-ASSETS_CSS="$(realpath $DIR/../assets/css)"
-ASSETS_JS="$(realpath $DIR/../assets/js)"
 
 LOG=$DIR/.installed
 
